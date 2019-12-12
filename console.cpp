b07c308f8137749a7e769cd528252b29efac9a2c
Andy Chiu <HandsomeAndyCT@gmail.com>
2019-12-12 21:53:40 +0800

    Console is now available
    The console have two functions which are equalivent to those from
    sample_console.py
    
    we use session id to access console and then we can get its host
    description from parser. The id used by the parser is the one come from
    QUERY_STRING, which is differ from sid.

--- console.cpp
+++ console.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <fstream>
 #include <boost/asio.hpp>
 #include <stdlib.h>
+#include <unistd.h>
 
+#define DEBUG
+#ifdef DEBUG
+#define DBGOUT(msgexpr){                        \
+    std::cout << "<!--" <<  msgexpr << "-->\n";   \
+}
+#else
+#define DBGOUT(msgexpr) {}
+#endif /* DEBUG */
+
+struct host {
+    int active;
+    unsigned short _port;
+    std::string host;
+    std::string port;
+    std::string file;
+};
 #define MAXHST 5
+
+
 class qstr_parser {
 private:
     enum state { ARG, VALUE, BAD };
@@ -37,13 +57,7 @@ private:
         return 0;
     }
 public:
-    struct host {
-        int active;
-        unsigned short _port;
-        std::string host;
-        std::string port;
-        std::string file;
-    } hst_list[MAXHST];
+    struct host hst_list[MAXHST];
     
     qstr_parser(char *query){
         qstr = strdup(query);
@@ -72,7 +86,7 @@ public:
                             exit(-1);
                         }
                         parse2host_arg(qstr+strst);
-                        std::cout << "arg = " << qstr+strst << '\n';
+                        DBGOUT("arg = " << qstr+strst);
                         stat = VALUE;
                         strst = i+1;
                     }
@@ -81,10 +95,10 @@ public:
                     if (qstr[i] == '&' || i == len) {
                         qstr[i] = '\0';
                         if (strst == i) {
-                            std::cout << "value = (empty)" << '\n';
+                            DBGOUT("value = (empty)")
                         } else {
                             parse2host_val(qstr+strst);
-                            std::cout << "value = " << qstr+strst << '\n';
+                            DBGOUT("value = " << qstr+strst)
                         }
                         stat = ARG;
                         strst = i+1;
@@ -108,29 +122,106 @@ public:
                 continue;
             }
             hst_list[i].active = 1;
-            using namespace std;
-            cout << "<pre>" << endl;
-            cout << "host " << i+1 << " is active" << endl;
-            cout << "name: " << hst_list[i].host << endl;
-            cout << "port: " << hst_list[i]._port << endl;
-            cout << "file: " << hst_list[i].file << endl;
-            cout << "</pre>" << endl;
+            DBGOUT("host " << i+1 << "is active");
+            DBGOUT("name: " << hst_list[i].host);
+            DBGOUT("port: " << hst_list[i]._port);
+            DBGOUT("file: " << hst_list[i].file);
+        }
+        return 0;
+    }
+};
+
+class console {
+private:
+    std::string html_head;
+    struct host *hst_list;
+    
+    int console_set_tbl(){
+        using namespace std;
+        cout << "<body>" << '\n';
+        cout << "<table class=\"table table-dark table-bordered\">" << '\n';
+        
+        cout << "<thead>" << '\n';
+        cout << "<tr>" << '\n';
+        for (size_t i = 0; i < nractive; i++) {
+            cout << "<th scope=\"col\">";
+            cout << session[i].hst->host;
+            cout << ":" << session[i].hst->_port;
+            cout << "</th>\n";
+        }
+        cout << "</tr>" << '\n';
+        cout << "</thead>" << '\n';
+        
+        cout << "<tbody>" << '\n';
+        cout << "<tr>" << '\n';
+        for (size_t i = 0; i < nractive; i++) {
+            cout << "<td><pre id=\"s";
+            cout << i << "\"";
+            cout << " class=\"mb-0\"></pre></td>\n";
+        }
+        cout << "</tr>" << '\n';
+        cout << "</tbody>" << '\n';
+        
+        cout << "</table>" << '\n';
+        cout << "</body>" << '\n';
+        return 0;
+    }
+    
+    int console_check_sid(int sid) {
+        if ( sid < 0 || sid >= nractive) {
+            return -1;
         }
         return 0;
     }
     
+public:
+    struct session {
+        int sid;
+        struct host *hst;
+    } session[MAXHST];
+    int nractive;
+    
+    void output_shell(int sid, std::string str) {
+        if (console_check_sid(sid))
+            return;
+        std::cout << "<script>document.getElementById('s";
+        std::cout << sid << "').innerHTML += '";
+        std::cout << str << "';</script>\n" << std::flush;;
+    }
+    
+    void output_command(int sid, std::string str) {
+        if (console_check_sid(sid))
+            return;
+        std::cout << "<script>document.getElementById('s";
+        std::cout << sid << "').innerHTML += '<b>";
+        std::cout << str << "&NewLine;</b>';</script>\n" << std::flush;
+    }
+    
+    console (struct host *hstp, std::string header_file){
+        using namespace std;
+        stringstream  buf;
+        hst_list = hstp;
+        nractive = 0;
+        memset(session, 0, MAXHST*sizeof(struct session));
+        
+        ifstream is (header_file, ifstream::binary);
+        buf << is.rdbuf();
+        cout << buf.str();
+        
+        for (size_t i = 0; i < MAXHST; i++) {
+            if (hst_list[i].active) {
+                session[nractive].sid = nractive;
+                session[nractive].hst = &hst_list[i];
+                nractive++;
+            }
+        }
+        
+        console_set_tbl();
+        cout << "</html>";
+    };
 };
 
-// // Friday afternoon + night
-// class console {
-// private:
-//     /* data */
-// 
-// public:
-//     console ();
-//     virtual ~console ();
-// };
-// 
+// // // Friday afternoon + night
 // class npshell_conn {
 // private:
 //     /* data */
@@ -148,7 +239,7 @@ int main(int argc, char const *argv[]) {
     std::cout << "\r\n";
     char *query_string = getenv("QUERY_STRING");
     
-    std::cout << "[" <<getpid() << "]"<<'\n';
+    DBGOUT("[" <<getpid() << "]");
     
     if (!query_string) {
         std::cerr << "error, no environment variable QUERY_STRING found" << '\n';
@@ -159,6 +250,7 @@ int main(int argc, char const *argv[]) {
     parse.start();
     parse.check();
     
+    console console(parse.hst_list, "console-head.html");
     
     return 0;
 }
